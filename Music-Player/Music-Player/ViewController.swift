c5eb32e211a224ff55f9272b10d64588d4640b5b
Allicia Viona <alliciawn@gmail.com>
2022-03-02 18:05:00 +0700

    Initial Commit

---
+++ Music-Player/Music-Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music-Player
+//
+//  Created by Allicia Viona Sagi on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
