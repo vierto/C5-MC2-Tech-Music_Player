e57cdc220ba98595c9bce921d2948bd1d8589203
Allicia Viona <alliciawn@gmail.com>
2022-03-16 08:00:06 +0700

    Edit lyrics function added

--- Music-Player/Music-Player/ViewController.swift
+++ Music-Player/Music-Player/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var arrOfMusicTitle: [String] = ["Happy","Daydreamer","Is This Love", "Auld Lang Syne", "10,000 Hours"]
     var arrOfMusicsinger: [String] = ["Pharrell Williams","AURORA","Aalia", "Denmark + Winter", "Dan + Shay & Justin Bieber"]
     var arrOfFavMusic: [Bool] = [false, false, false, false, false]
+    var arrOfLyrics: [String] = ["Huh (Because I'm happy) Clap along if you feel like a room without a roof (Because I'm happy) Clap along if you feel like happiness is the truth (Because I'm happy) Clap along if you know what happiness is to you", "And we become night time dreamers Street walkers and small talkers When we should be daydreamers And moonwalkers and dream talkers And we become night time dreamers Street walkers, small talkers When we should be daydreamers And moonwalkers and dream talkers In real life", "A blue bird in my heart Why do you try to get it out It's sad It's sad to know you Don't take out my blue bird It only knows how to stay dark Don't spoil It shouldn't hurt no more","Should auld acquaintance be forgot And never brought to mind? Should auld acquaintance be forgot And days auld lang syne? For auld lang syne, my dear For auld lang syne We'll take a cup of kindness yet For auld lang syne","I'd spend 10, 000 hours and 10, 000 more Oh, if that's what it takes to learn that sweet heart of yours And I might never get there, but I'm gonna try If it's 10, 000 hours or the rest of my life I'm gonna love you (Ooh, ooh-ooh, ooh, ooh) Do you miss the road that you grew up on? Did you get your middle name from your grandma? When you think about your forever now Do you think of me?"]
     
     /// Data Control
     var currMusic = 0
@@ -22,9 +23,12 @@ class ViewController: UIViewController {
     /// Outlets
     @IBOutlet weak var imageCoverImage: UIImageView!
     
+    @IBOutlet weak var lyricsTextView: UITextView!
+    
+    @IBOutlet weak var musicDurationSlider: UISlider!
+    
     @IBOutlet weak var musicTitleLbl: UILabel!
     @IBOutlet weak var musicSingerLbl: UILabel!
-    @IBOutlet weak var musicDurationSlider: UISlider!
     @IBOutlet weak var currTimeLbl: UILabel!
     @IBOutlet weak var timeLeftLbl: UILabel!
     
@@ -41,6 +45,9 @@ class ViewController: UIViewController {
         // Set up the first music cover
         imageCoverImage.image = arrOfMusicCover[currMusic]
         
+        // Set up lyrics
+        lyricsTextView.text = arrOfLyrics[0]
+        
         // Check first music fave status
         if arrOfFavMusic[0] {
             favMusicBtn.setImage(UIImage(systemName: "heart.fill"), for: .normal)
@@ -50,22 +57,28 @@ class ViewController: UIViewController {
     }
     
     @IBAction func pressNext(_ sender: Any) {
+        // Save changes if there's any change to the lyrics
+        saveUpdatedLyrics()
+        
+        // Validate if it's already on the last page
         if currMusic ==  arrOfMusicCover.count-1 {
             currMusic = 0
         } else {
             currMusic += 1
         }
-        
         changeMusic()
     }
     
     @IBAction func pressPrev(_ sender: Any) {
+        // Save changes if there's any change to the lyrics
+        saveUpdatedLyrics()
+        
+        // Validate if it's on the very first page
         if currMusic > 0 {
             currMusic -= 1
         } else {
             currMusic = 0
         }
-        
         changeMusic()
     }
     
@@ -95,8 +108,13 @@ class ViewController: UIViewController {
     }
     
     func changeMusic() {
+        lyricsTextView.text = arrOfLyrics[currMusic]
         imageCoverImage.image = arrOfMusicCover[currMusic]
         changeHeartIcon()
+    }
+    
+    func saveUpdatedLyrics() {
+        arrOfLyrics[currMusic] = lyricsTextView.text
     }
     
     func changeHeartIcon() {
